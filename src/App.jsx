48682d77f4f9a67f5e31b383b15ea92fe665366e
ArticSaber <kaarthiknirmala@gmail.com>
2023-02-07 08:16:57 +0530

    idk

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,8 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import "./App.css";
-
 function square(e) {
   const [count, setCount] = useState(0);
-
   return <h1>XO</h1>;
+
 }
 
 export default App;
